e7d676914b542e707eb396969062f6d2f2c5a47e
Maria Huerta <Malena@maria-huertas-macbook-pro.local>
2017-08-28 19:05:54 -0700

    Created a Basic Card module

---
+++ BasicCard.js
@@ -0,0 +1,14 @@
+var BasicCard = function(front,back){
+    this.front= front;
+    this.back=back;
+
+    this.displayCard = function(){
+        console.log(this.front);
+        console.log(this.back);
+    }
+}
+var abasicCard= new BasicCard("What was the first president of the U.S.","George Washington");
+
+abasicCard.displayCard();
+// Export the module
+module.exports= BasicCard;
